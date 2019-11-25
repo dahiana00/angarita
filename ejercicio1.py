6349e5125557f1a558c384c7403c79b5151d9dba
Dahiana <58174479+dahiana00@users.noreply.github.com>
2019-11-25 09:05:49 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,17 @@
+num1=int(input("ingresa el primer numero "))
+num2=int(input("ingresa el segundo numero "))
 
+
+def calcular_numeros(num1,num2):
+    if num1>num2:
+       print("el primer numero es mayor")
+       return num1
+        
+    else:
+         if  num2>num1:
+             print("el segundo numero es mayor")
+
+             return num2
+
+mensaje=calcular_numeros(num1,num2)         
+print(mensaje)  
