02fed91ae67fd49df479fa35417bb5aa1bcc238b
Dahiana <58174479+dahiana00@users.noreply.github.com>
2019-11-25 09:07:43 -0500

    Create ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,31 @@
+#ciclo for
+arregloNumeros=[1,2,3,4,5,6,7,8,9,10]
+arregloNumeros=[1] 
+arregloNumeros=[2]
+arregloNumeros=[3]
+arregloNumeros=[4]
+arregloNumeros=[5]
+arregloNumeros=[6]
+arregloNumeros=[7]
+arregloNumeros=[8]
+arregloNumeros=[9] 
+arregloNumeros=[10]
+for i in range (1,10):
+    print(i)
+
+     #ciclo while
+j=0
+while j<10:
+    arreglo2Numeros=[1,2,3,4,5,6,7,8,9,10]
+    arreglo2Numeros=[1] 
+arreglo2Numeros=[2]
+arreglo2Numeros=[3]
+arreglo2Numeros=[4]
+arreglo2Numeros=[5]
+arreglo2Numeros=[6]
+arreglo2Numeros=[7]
+arreglo2Numeros=[8]
+arreglo2Numeros=[9] 
+arreglo2Numeros=[10]
+print(j)
+   
